6b9701430fa35720aaab9f8747b86bf11597d0a3
Ranveer225 <singr12@farmingdale.edu>
2024-11-24 17:07:56 -0500

    Make settings functional and run app

---
+++ src/main/java/model/SettingsLauncher.java
@@ -0,0 +1,24 @@
+package model;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SettingsLauncher extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/musicresources/settings.fxml"));
+        Parent root = loader.load();
+
+        primaryStage.setTitle("Settings");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
